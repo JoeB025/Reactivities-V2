f12e92b1988e72dc9318db91054a4e2143c72b12
Joseph Scott-Bentley <jscottbentley@hotmail.com>
2025-12-09 10:28:33 +0000

    End of section 8

---
+++ client/src/features/activities/details/ActivityDetailPage.tsx
@@ -0,0 +1,34 @@
+import { Typography, Grid2 } from "@mui/material"
+import { useParams } from "react-router";
+import { useActivities } from "../../../lib/hooks/useActivities";
+import ActivityDetailsSidebar from "./ActivityDetailsSidebar";
+import ActivityDetailsHeader from "./ActivityDetailsHeader";
+import ActivityDetailsInfo from "./ActivityDetailsInfo";
+import ActivityDetailsChat from "./ActivityDetailsChat";
+
+
+export default function ActivityDetailPage() {
+
+  const {id} = useParams(); 
+  const {activity, isLoadingActivity} = useActivities(id); 
+
+if (isLoadingActivity) return <Typography>Loading...</Typography>
+if (!activity) return <Typography>Activity not found</Typography>
+
+  return (
+    <Grid2 container spacing={3}>
+      <Grid2 size={8}>
+        <ActivityDetailsHeader activity={activity} />
+        <ActivityDetailsInfo activity={activity} />
+        <ActivityDetailsChat />
+      </Grid2>
+      <Grid2 size={4}>
+        <ActivityDetailsSidebar />
+      </Grid2>
+    </Grid2>
+  )
+}
+
+
+// This file is a child of the <ActivityDashboard /> The activities data is again passed down as props from <ActivityDashboard /> to here. 
+// obviously <ActivityDashboard /> gets them passed down from App.tsx 
